a0a3d2b69567a51877f87918a7cffafde62a71e2
RKamey <90283664+RKamey@users.noreply.github.com>
2022-03-12 17:50:00 -0600

    interpolación cuadrática

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 function refresh() {
     formulario.reset();
 }
-// let limpiarV = document.getElementById("refresh")
+function refreshC() {
+    formularioC.reset();
+}
 
-// limpiarV.addEventListener("click", function () {
-//     location.reload();
-// })
+function lineal() {
+    let FX0 = parseFloat(document.getElementById("fx0").value);
+    let FX1 = parseFloat(document.getElementById("fxLineal").value);
+    let X0 = parseFloat(document.getElementById("x0").value);
+    let X1 = parseFloat(document.getElementById("x1").value);
+    let X = parseFloat(document.getElementById("x").value);
+
+    let result = (FX0 + (FX1 - FX0) / (X1 - X0)) * (X - X0);
+
+    parseFloat(document.getElementById("resultado").value = result);
+    return result;
+}
+
+function cuadratica() {
+
+}
