465332fbddecd2d56aebf1c511c711fa554bb668
eljorgecruz <90343862+eljorgecruz@users.noreply.github.com>
2022-03-13 00:25:07 -0600

    Add files via upload
    Funcion de error porcentual en interpolacion lineal y cuadratica

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function refreshC() {
 
 function lineal() {
     let FX0 = parseFloat(document.getElementById("fx0").value);
-    let FX1 = parseFloat(document.getElementById("fxLineal").value);
+    let FX1 = parseFloat(document.getElementById("fx1").value);
     let X0 = parseFloat(document.getElementById("x0").value);
     let X1 = parseFloat(document.getElementById("x1").value);
     let X = parseFloat(document.getElementById("x").value);
@@ -18,12 +18,20 @@ function lineal() {
     return result;
 }
 
-function errorRPL() {
-    let valorVerdadero = parseFloat(document.getElementById("fxLineal").value);
-    let errorVerdadero = valorVerdadero - lineal();
-    let errorPorcentual = `${(errorVerdadero / valorVerdadero) * 100} %`
+function errorRL() {
+    let efe0 = parseFloat(document.getElementById("fx0").value);
+    let efe1 = parseFloat(document.getElementById("fx1").value);
+    let ekis0 = parseFloat(document.getElementById("x0").value);
+    let ekis1 = parseFloat(document.getElementById("x1").value);
+    let ekis = parseFloat(document.getElementById("x").value);
+    let lineal = (efe0 + (efe1 - efe0) / (ekis1 - ekis0)) * (ekis - ekis0);
+    //let prueba = 10;
+    let valorVerdadero = parseFloat(document.getElementById("valorV").value);
+    let errorVerdadero = valorVerdadero - lineal;
+    let errorPorcentual = (errorVerdadero / valorVerdadero) * 100;
 
-    parseFloat(document.getElementById("errorRPL").value = errorPorcentual)
+    parseFloat(document.getElementById("errorRPL").value = errorPorcentual);
+    return errorPorcentual;
 }
 
 function cuadratica() {
@@ -45,5 +53,24 @@ function cuadratica() {
 }
 
 function errorRPC() {
+    let fx0 = parseFloat(document.getElementById("fx00").value);
+    let fx1 = parseFloat(document.getElementById("fx01").value);
+    let fx2 = parseFloat(document.getElementById("fx02").value);
+    let x0 = parseFloat(document.getElementById("x00").value);
+    let x1 = parseFloat(document.getElementById("x01").value);
+    let x2 = parseFloat(document.getElementById("x02").value);
+    let x = parseFloat(document.getElementById("xx").value);
     
+    let b0 = fx0;
+    let b1 = (fx1 - fx0) / (x1 - x0);
+    let b2 = (((fx2 - fx0) / (x2 - x0)) - (b1)) / (x2 - x1);
+    let resultado = (b0 + (b1 * (x - x0)) + (b2 * (x - x0) * (x-x1)));
+
+    let valorVerdadero = parseFloat(document.getElementById("vVcua").value);
+    let errorVerdadero = valorVerdadero - resultado;
+
+    let errorPorcentual = (errorVerdadero / valorVerdadero) * 100;
+
+    parseFloat(document.getElementById("eRPC").value = Math.abs(errorPorcentual));
+    return Math.abs(errorPorcentual);
 }
